2e2804ae3385632cd320b3f5d34300bc24642346
myeonsei <38792588+myeonsei@users.noreply.github.com>
2018-09-19 21:10:16 +0900

    Create Session02 Basic Python.py

---
+++ Quest/Session02 Basic Python.py
@@ -0,0 +1,13 @@
+# 1
+# https://programmers.co.kr/learn/courses/30/lessons/12907
+# 위 링크의 문제를 풀어주세요.
+
+
+# 2
+# 짝수인 원소에 대응하는 인덱스의 합을 리턴하는 함수 f4()를 만들어주세요. 단, 제어문(for, while, if)을 사용하지 마세요!
+# Hint : map, lambda, filter, enumerate
+
+
+# 3
+# diamonds_data.csv 의 열을 뒤집은 파일을 생성하는 코드를 만들어주세요.
+# (carat, depth, table, price, x, y, z) => (z, y, x, price, table, depth, carat)
