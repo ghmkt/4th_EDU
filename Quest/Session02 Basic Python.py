6e97cd335f58dffd03df7b9cba6da2391f081eb0
Yang Youngjin <41133006+ojin0611@users.noreply.github.com>
2018-09-26 00:39:01 +0900

    양영진 퀘스트2 풀이
    1번: 효율성 해결 포기!!
    2번: 문제 이해와 예시 답안 이해가 잘 안돼요
    3번: 첫 열이 남는 건 봐주세요 ;ㅅ;

--- Quest/Session02 Basic Python.py
+++ Quest/Session02 Basic Python.py
@@ -1,7 +1,33 @@
 # 1
 # https://programmers.co.kr/learn/courses/30/lessons/12907
 # 위 링크의 문제를 풀어주세요.
+def solution(n, money):
+    answer=0
+    if len(money)==1:
+        if n%money[0]==0:
+            return 1
+    
+    elif len(money)==2:
+        mylist = []
+        small=money[0]
+        large=money[-1]
+        
+        for i in range(int(n/large)+1):
+            mylist.append(n-i*large)
+        answer = sum(list(map(lambda x: x%small==0, mylist)))
 
+        
+    else: # len(money)>2
+        mylist = []
+        large=money[-1]
+        new_money = money[:len(money)-1]
+
+        for i in range(int(n/large)+1):
+            answer= answer+solution(n-i*large,new_money)
+    
+    return answer
+
+# solution(5,[1,2,5])
 
 # 2
 # 짝수인 원소에 대응하는 인덱스의 합을 리턴하는 함수 f4()를 만들어주세요. 단, 제어문(for, while, if)을 사용하지 마세요!
