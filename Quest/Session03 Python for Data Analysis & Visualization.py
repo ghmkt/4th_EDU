cffb4ce14fde31dfd36a245b1519be0a1d9bfcaf
Dong-Keon, Oh <40379446+ho2921ho@users.noreply.github.com>
2018-09-28 23:32:24 +0900

    Update Session03 Python for Data Analysis & Visualization.py

--- Quest/Session03 Python for Data Analysis & Visualization.py
+++ Quest/Session03 Python for Data Analysis & Visualization.py
@@ -3,11 +3,11 @@
 import pandas as pd 
 df = pd.read_csv("sex ratio.csv")
 
-df.rename(columns = {"Unnamed: 0"  : "지역"}, inplace = True) # 빈 컬럼명을 "지역"으로 할당
+df.rename(columns = {"Unnamed: 0"  : "지역"}, inplace = True) # 빈 컬럼명을 "지역"으로 변경
 df2 = pd.melt(df,id_vars = ["지역"], var_name ="연도", value_name = "성비" ) # wide 데이터를 long 데이터로 변환
 df2 = df2.sort_values(["지역","연도"]) # 지역과 연도로 재정렬
-df2 = df2.reset_index(drop = True) # 새로운 인덱스로 갱신
-df2.to_csv("new sex ratio.csv", encoding= "EUC - KR") # 변화된 파일 저장
+df2 = df2.reset_index(drop = True) # 기존 인덱스를 새로운 인덱스로 갱신
+df2.to_csv("new sex ratio.csv", encoding= "EUC - KR") # 변환된 파일 저장
 
 ### 2.‘province data.csv’로부터 지역별로 전 기간에 걸친 평균 gdp와 unemploymen를 구한 뒤, 이를 산포도로 표현하세요.
 ### 단, 평균 성비가 110 이상인 지역의 산포도는 파란색 X 모양의 점, 110 미만인 지역의 산포도는 빨간색 세모 모양의 점으로 표현하세요.
