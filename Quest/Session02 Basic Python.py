7b7d3ead9bce9f643d22e19ad064a5d403511586
GrowthHackers <37243111+ghmkt@users.noreply.github.com>
2018-09-28 14:14:27 +0900

    Add some comments to Session02 Basic Python.py

--- Quest/Session02 Basic Python.py
+++ Quest/Session02 Basic Python.py
@@ -11,17 +11,23 @@ def solution(n, money):
       res[j] += res[j-i]
   return res[n]
 
+# http://gomoveyongs.tistory.com/m/65 에 이해하기 쉽게 설명이 되어 있습니다.
+# 풀이 1번은 dynamic programming을 이용하여 풀되, 2차원 행렬이 아닌 1차원 리스트에서 경우의 수를 업데이트 했습니다.
+
+
 # 풀이 2번
-def solution(n, money): # 재귀함수 이용
-  if len(money) == 1:
+def solution(n, money):     # 재귀함수 이용
+  if len(money) == 1:       # base case : 동전의 종류가 하나일 때, n이 동전의 단위로 나누어 떨어지면 1, 아니면 0
     if n%money[0] == 0:
-      return 1
+      return 1              
     else:
       return 0
   else:
     res = 0
-    for i in range(n//money[0]+1):
-      res += solution(n-i*money[0], money[1:])
+    for i in range(n//money[0]+1):              # 이 for문에서 money[0]를 사용하는 경우는 모두 생각하고 남은 금액은 다른 동전들이 해결하도록 
+                                                # solution()에게 넘겨 줄 것이다.
+      res += solution(n-i*money[0], money[1:])  # money[0]를 i번 만 사용하고 남은 금액은 money[1:]이 해결하도록 하면, 
+                                                # 언젠가 base case가 되어 리턴될 것이다.
     return res
   
 
@@ -32,6 +38,7 @@ def solution(n, money): # 재귀함수 이용
 def f4(lst):
     return sum(map(lambda x: x[0], filter(lambda x:x[1]%2==1, enumerate(lst))))
 
+  
 # 3
 # diamonds_data.csv 의 열을 뒤집은 파일을 생성하는 코드를 만들어주세요.
 # (carat, depth, table, price, x, y, z) => (z, y, x, price, table, depth, carat)
