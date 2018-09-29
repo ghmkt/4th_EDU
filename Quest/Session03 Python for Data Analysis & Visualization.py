8d7418e778c608f03a3c493163b7b8424568ad0f
Yang Youngjin <41133006+ojin0611@users.noreply.github.com>
2018-09-29 15:35:24 +0900

    Session03 양영진
    1번까지 제출

--- Quest/Session03 Python for Data Analysis & Visualization.py
+++ Quest/Session03 Python for Data Analysis & Visualization.py
@@ -3,6 +3,33 @@
 
 # 1
 # numpy, pandas를 활용하여, ‘sex ratio.csv’로부터 아래와 같은 형태를 가진 ‘new sex ratio.csv’를 반환하는 코드를 구현해주세요.
+import numpy as np
+import pandas as pd
+df = pd.read_csv('sex ratio.csv', engine = 'python', index_col = 0)
+
+# 성비
+dfv=df.values
+# dfv.shape # 열 몇개? # [0] : 몇개의 행?
+sexratio=dfv.reshape((192))
+
+# 지역 설정
+regionlist=[]
+for i in range(dfv.shape[0]):
+    region=df.index.values[i]
+    for j in range(dfv.shape[1]-1):
+        region=np.append(region,df.index.values[i])
+    regionlist=np.append(regionlist,region)
+# len(regionlist)    
+
+# 연도 설정
+yearlist=df.columns.values
+for i in range(dfv.shape[0]-1):
+    yearlist=np.append(yearlist,df.columns.values)
+# len(yearlist)
+
+# 새 데이터프레임 제작
+new_df = pd.DataFrame({'지역':regionlist, '연도':yearlist, '성비':sexratio})
+new_df.to_csv('new sex ratio.csv')
 
 
 # 2
