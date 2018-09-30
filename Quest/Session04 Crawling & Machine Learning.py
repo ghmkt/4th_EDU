355672ce19accfc5f8888d1119481bfe434ab1ce
myeonsei <38792588+myeonsei@users.noreply.github.com>
2018-10-01 01:38:58 +0900

    Update and rename Session04_Crawling.py to Session04 Crawling & Machine Learning.py

---
+++ Quest/Session04 Crawling & Machine Learning.py
@@ -0,0 +1,17 @@
+# Quest 제출 시 제목을 'Session04 이름'으로 해주세요. ex. Session04 이재경
+
+
+# Crawling Quest
+# http://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=100&sid2=269
+# 위의 주소는 네이버 정치 일반 기사 페이지의 주소입니다. 한 페이지에는 20개의 기사가 있습니다. 
+# 최신 5페이지에 걸쳐 100개의 제목과 기사를 가져올 수 있는 코드를 작성해주세요.
+
+
+# Machine Learning Quest는 아래 1, 2 중 택1입니다.
+
+# Machine Learning Quest 1
+# 'caschool.csv' 데이터에서 str과 avginc변수를 통해 read_scr을 예측할 수 있는 y=a*x1+b*x2+c 모형으로 Cost function을 minimize해보세요.
+
+# Machine Learning Quest 2
+# 앤드류 강의 week1과 week2에서 필요한 강의 듣고 중요 내용 요약해서 올리기
+# https://www.coursera.org/learn/machine-learning
