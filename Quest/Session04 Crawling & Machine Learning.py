6547eb6e84db8b71265c3c6538046ca9f74a6475
DahamLee <daham1224@gmail.com>
2018-10-06 12:18:21 +0900

    Update Session04 Crawling & Machine Learning.py

--- Quest/Session04 Crawling & Machine Learning.py
+++ Quest/Session04 Crawling & Machine Learning.py
@@ -12,6 +12,73 @@
 # Machine Learning Quest 1
 # 'caschool.csv' 데이터에서 str과 avginc변수를 통해 read_scr을 예측할 수 있는 y=a*x1+b*x2+c 모형으로 Cost function을 minimize해보세요.
 
+import pandas as pd
+import numpy as np
+
+def predict(x_i, beta):
+    # N * 1 vector
+    result = np.dot(x_i, beta)
+    return result
+
+def error(x_i, y_i, beta):
+    # N * 1 vector
+    return y_i - predict(x_i, beta)
+
+def squared_error(x_i, y_i, beta):
+    # 1 * 1 Matrix
+    return np.dot(error(x_i, y_i, beta).T, error(x_i, y_i, beta))
+
+def squared_error_gradient(x_i, y_i, beta):
+    # K * 1 vector
+    return -2 * np.dot(x_i.T, error(x_i, y_i, beta))
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, beta_0, alpha_0):
+    data = np.concatenate((x, y.reshape(-1,1)), axis=1)
+    beta = beta_0
+    alpha = alpha_0
+    min_beta, min_error = None, np.inf
+    iterations_with_no_improvement = 0
+    
+    print('initial error: ', target_fn(x, y, beta))
+    while iterations_with_no_improvement < 1000:
+        
+        error = target_fn(x, y, beta)
+        
+        if error < min_error:
+            min_beta, min_error = beta, error
+            iterations_with_no_improvement += 1
+            alpha = alpha_0
+        else:
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+            
+        print('beta: ', beta)
+        print('gradient: ', gradient_fn(x, y, beta))
+        print('error: ', error)
+        beta = beta - (alpha * gradient_fn(x, y, beta))
+        
+    return min_beta
+
+
+def estimate_beta(x, y, alpha):
+    beta_initial = [np.random.random() for x_i in x[0]]
+    return minimize_stochastic(squared_error, squared_error_gradient, x, y, beta_initial, alpha)
+
+cashschool = pd.read_csv('/Users/daham/Desktop/GrowthHackers/4th-EDU/Session/caschool.csv')
+csv_data = cashschool.loc[:, ['str', 'avginc']]
+x = np.array(csv_data)
+y = np.array(cashschool.read_scr)
+
+print('정답: ', estimate_beta(x,y, 0.000001))
+
+import statsmodels.api as sm
+model = sm.OLS(y, x)
+results = model.fit()
+results.summary()
+
+# OLS 한 것과 같은 Coefficient 출력 확인 완료
+
+
 # Machine Learning Quest 2
 # 앤드류 강의 week1과 week2에서 필요한 강의 듣고 중요 내용 요약해서 올리기
 # https://www.coursera.org/learn/machine-learning
