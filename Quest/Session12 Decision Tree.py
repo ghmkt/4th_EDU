69d9670fa393f772cce316d4767f4dae59aea316
GrowthHackers <37243111+ghmkt@users.noreply.github.com>
2018-10-27 23:47:18 +0900

    Create Session12 Decision Tree.py

---
+++ Quest/Session12 Decision Tree.py
@@ -0,0 +1,4 @@
+# 제출 시 파일명은 Session12 이름 으로 해주세요.
+
+# 1. 실습 코드를 활용하여 [scikit-learn의 기본 데이터 breast_cancer]에서 RandomForest를 활용해 양성, 악성을 예측 => 코드 + confusion matrix 캡쳐본 첨부
+# 2. 실습 코드를 활용하여 [scikit-learn의 기본 데이터 breast_cancer]에서 Boosting을 활용해 양성, 악성을 예측 => 코드 + confusion matrix 캡쳐본 첨부
