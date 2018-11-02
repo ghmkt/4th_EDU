58b2383d236a7a9561b49b76726960c39954e101
woojin716 <43376860+woojin716@users.noreply.github.com>
2018-11-02 09:36:22 +0900

    Update Session04 Crawling & Machine Learning.py

--- Quest/Session04 Crawling & Machine Learning.py
+++ Quest/Session04 Crawling & Machine Learning.py
@@ -5,6 +5,39 @@
 # http://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=100&sid2=269
 # 위의 주소는 네이버 정치 일반 기사 페이지의 주소입니다. 한 페이지에는 20개의 기사가 있습니다. 
 # 최신 5페이지에 걸쳐 100개의 제목과 기사를 가져올 수 있는 코드를 작성해주세요.
+from bs4 import BeautifulSoup as bs             
+import urllib.request as req                    
+from urllib.request import urlopen
+import pandas as pd                              
+import datetime                                 
+import re   
+
+articles = []
+for i in range(1,6):
+    url = "https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2=269&sid1=100&mid=shm&date=20181102&page=" + str(i)
+    html = urlopen(url)
+    soup = bs(html,"lxml",from_encoding='utf-8')
+    articles.extend(soup.findAll("dt"))
+     
+url_list = []
+for link in articles:
+    url_list.append(link.find("a")["href"])
+    
+url_list2 = list(set(url_list)) ##중복 제거
+url_list2
+print(len(url_list2))
+
+df = pd.DataFrame(columns = ['기사제목','내용'])
+
+for i in range(len(url_list2)):
+    html2 = urlopen(url_list2[i])
+    content = soup.find("div",{"id":"articleBodyContents"})
+    title = soup.find("h3",{"id":"articleTitle"}).find(text=True)
+    result = hangul.sub('',str(content))
+    result = re.sub('본문 내용  플레이어   플레이어    오류를 우회하기 위한 함수 추가','',result)
+    result = re.sub('본문 내용  플레이어   동영상 뉴스       영상 플레이어   플레이어    오류를 우회하기 위한 함수 추가','',result)
+    df = df.append({'Title':title, 'Article':result}, ignore_index = True())
+
 
 
 # Machine Learning Quest는 아래 1, 2 중 택1입니다.
