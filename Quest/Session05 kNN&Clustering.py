54fbe6016a831aa67444397a563e7c492f450170
woojin716 <43376860+woojin716@users.noreply.github.com>
2018-11-02 14:12:12 +0900

    Update Session05 kNN&Clustering.py

--- Quest/Session05 kNN&Clustering.py
+++ Quest/Session05 kNN&Clustering.py
@@ -22,12 +22,38 @@ def return_vector(filename):
 print(return_vector("0_23.txt"))
 
 # 2. 파일명을 인자로 받아서 Y값을 반환하는 함수를 정의하세요.
-
+def return_y(filename):
+    yy = filename.split('.')[0]
+    y = int(yy.split('_')[0])
+    return y
 
 # 3. 데이터를 X_train, y_train, X_test, y_test로 나누세요.
-
+X=[]
+y=[]
+for i in range(len(training_file_list)):
+    X.append(return_vector(training_file_list[i]))
+    y.append(return_y(training_file_list[i]))
+print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+print(len(X_train), len(X_test), len(y_train), len(y_test))
 
 # 4. 1~10의 k에 대해 KNN을 시행하고 테스트셋에 대한 스코어를 그래프로 그리세요 (sklearn 이용)
+accuracy=[]
+for k in range(1,11):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    test_pred = knn.predict(X_test)
+    accuracy = np.mean(np.array(y_test).astype(np.int32)== test_pred)
+    print(k, ":", accuracy)
 
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.plot(range(1,11),accracy)
 
 # 5. 최적의 K로 testdata에 넣어 정확도 측정
+
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(X_train, y_train)
+test_pred = knn.predict(X_test)
+accuracy = np.mean(np.array(y_test).astype(np.int32)== test_pred)
+accuracy
