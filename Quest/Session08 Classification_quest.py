4b42c2ffff46918a39e347be9164bf7ddbc9f933
Edwinpark <43375723+Edwinpark@users.noreply.github.com>
2018-11-03 02:21:37 +0900

    Session08 박의연

--- Quest/Session08 Classification_quest.py
+++ Quest/Session08 Classification_quest.py
@@ -17,25 +17,31 @@ def loadDataSet():
                  ['coupon','promo','code','here']]
     classVec = [0,1,1,1,0,0,0,0,0,0,1,1,0,1]    #1 is spam, 0 not
     return postingList,classVec
-                 
+# 각 14개의 문장에서 어떤 걸 스팸으로 볼지 안볼지를 1과 0으로 지정해줌.
+
 def createVocabList(dataSet):
     vocabSet = set([])  #create empty set
     for document in dataSet:
         vocabSet = vocabSet | set(document) #union of the two sets
     return list(vocabSet)
 
+
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
-        else: print "the word: %s is not in my Vocabulary!" % word
+        else: print ("the word: %s is not in my Vocabulary!" )% word
+            # 파이썬 3버전이므로 괄호를 입혀줍니다.
     return returnVec
+# 삽입하는 데이터를 document에 넣고 word2vec을 실행하여 단어간 유사성을 봅니다.
+
 
 def trainNB00(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pSpam = sum(trainCategory)/float(numTrainDocs)
+    # trainset의 단어의 개수와 스팸일 확률
     p0Num = zeros(numWords); p1Num = zeros(numWords)
     p0Denom = 0.0; p1Denom = 0.0
     for i in range(numTrainDocs):
@@ -45,9 +51,10 @@ def trainNB00(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num/p1Denom
-    p0Vect = p0Num/p0Denom
-    return p0Vect,p1Vect,pSpam
+    p1Vect = p1Num/p1Denom #스팸일때(p1일때) 그 단어가 있을 확률
+    p0Vect = p0Num/p0Denom #스팸이아닐때(p0일때) 그 단어가 있을 확률
+    return p0Vect,p1Vect,pSpam # 위의2개와 스팸일 확률 3개의 확률을 보여줌.
+
 
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
@@ -65,6 +72,7 @@ def trainNB0(trainMatrix,trainCategory):
     p1Vect = log(p1Num/p1Denom)
     p0Vect = log(p0Num/p0Denom)
     return p0Vect,p1Vect,pSpam
+# p0Denom과 p1Denom의 디폴트를 2로올려 모델을 변경
 
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
@@ -73,13 +81,17 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
         return 1
     else: 
         return 0
-    
+# p1과 p0를 위와같은 모델로 설정하고, p1이 p0보다 크면 스팸, 작으면 no 스팸임을 정의해주는 함수
+
+
 def bagOfWords2VecMN(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] += 1
     return returnVec
+#
+
 
 def testingNB():
     listOPosts,listClasses = loadDataSet()
@@ -90,116 +102,22 @@ def testingNB():
     #print(str(trainMat))
     p0V,p1V,pSpam = trainNB0(array(trainMat),array(listClasses))
 
-    testEntry = ['free', 'pizza', 'coupon']
+    testEntry = ['free', 'pizza', 'coupon'] #free pizza coupon 이 3가지 단어를 얘네가 있으면 스팸, 없으면 스팸x인 기준으로 설정하고자함.
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
 
     if classifyNB(thisDoc, p0V, p1V, pSpam) == 1:
-        print testEntry, 'classified this is a spam'
+        print (testEntry, 'classified this is a spam')
     else:
-        print testEntry, 'classified this is not a spam'
-
+        print (testEntry, 'classified this is not a spam')
+#그래서 myvocablist에 test entry 세단어가 관련이 되어있으면 스팸이라 print 없으면 스팸이 아니라고 print해줌.
     testEntry = ['I', 'will', 'miss','free', 'pizza']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
 
     if classifyNB(thisDoc,p0V,p1V,pSpam) == 1:
-        print testEntry, 'classified this is a spam'
+        print (testEntry, 'classified this is a spam')
     else:
-        print testEntry, 'classified this is not a spam'
+        print (testEntry, 'classified this is not a spam')
+# 만들어진 모델에 free~ classified~ 2개의 문장을 넣어 테스트하여 그것이 spam인지 아닌지 판별.       
 
-
-def textParse(bigString):    #input is big string, #output is word list
-    import re
-    listOfTokens = re.split(r'\W*', bigString)
-    return [tok.lower() for tok in listOfTokens if len(tok) > 2] 
-    
-def spamTest():
-    docList=[]; classList = []; fullText =[]
-    for i in range(1,26):
-        wordList = textParse(open('email/spam/%d.txt' % i).read())
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(1)
-        wordList = textParse(open('email/ham/%d.txt' % i).read())
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(0)
-    vocabList = createVocabList(docList)#create vocabulary
-    trainingSet = range(50); testSet=[]           #create test set
-    for i in range(10):
-        randIndex = int(random.uniform(0,len(trainingSet)))
-        testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])  
-    trainMat=[]; trainClasses = []
-    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
-        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
-        trainClasses.append(classList[docIndex])
-    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
-    errorCount = 0
-    for docIndex in testSet:        #classify the remaining items
-        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
-        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
-            errorCount += 1
-            print "classification error",docList[docIndex]
-    print 'the error rate is: ',float(errorCount)/len(testSet)
-    #return vocabList,fullText
-
-def calcMostFreq(vocabList,fullText):
-    import operator
-    freqDict = {}
-    for token in vocabList:
-        freqDict[token]=fullText.count(token)
-    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True) 
-    return sortedFreq[:30]       
-
-def localWords(feed1,feed0):
-    import feedparser
-    docList=[]; classList = []; fullText =[]
-    minLen = min(len(feed1['entries']),len(feed0['entries']))
-    for i in range(minLen):
-        wordList = textParse(feed1['entries'][i]['summary'])
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(1) #NY is class 1
-        wordList = textParse(feed0['entries'][i]['summary'])
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(0)
-    vocabList = createVocabList(docList)#create vocabulary
-    top30Words = calcMostFreq(vocabList,fullText)   #remove top 30 words
-    for pairW in top30Words:
-        if pairW[0] in vocabList: vocabList.remove(pairW[0])
-    trainingSet = range(2*minLen); testSet=[]           #create test set
-    for i in range(20):
-        randIndex = int(random.uniform(0,len(trainingSet)))
-        testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])  
-    trainMat=[]; trainClasses = []
-    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
-        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
-        trainClasses.append(classList[docIndex])
-    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
-    errorCount = 0
-    for docIndex in testSet:        #classify the remaining items
-        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
-        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
-            errorCount += 1
-    print 'the error rate is: ',float(errorCount)/len(testSet)
-    return vocabList,p0V,p1V
-
-def getTopWords(ny,sf):
-    import operator
-    vocabList,p0V,p1V=localWords(ny,sf)
-    topNY=[]; topSF=[]
-    for i in range(len(p0V)):
-        if p0V[i] > -6.0 : topSF.append((vocabList[i],p0V[i]))
-        if p1V[i] > -6.0 : topNY.append((vocabList[i],p1V[i]))
-    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
-    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
-    for item in sortedSF:
-        print item[0]
-    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
-    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
-    for item in sortedNY:
-        print item[0]
 
 testingNB()
