72000be90c8d62c67b9d704e913c65a9adc9975a
Hyuna Lee <43376842+hysophie@users.noreply.github.com>
2018-11-02 16:47:43 +0900

    Update Session03 Python for Data Analysis & Visualization.py

--- Quest/Session03 Python for Data Analysis & Visualization.py
+++ Quest/Session03 Python for Data Analysis & Visualization.py
@@ -4,7 +4,58 @@
 # 1
 # numpy, pandas를 활용하여, ‘sex ratio.csv’로부터 아래와 같은 형태를 가진 ‘new sex ratio.csv’를 반환하는 코드를 구현해주세요.
 
+# csv파일 불러오기
+import pandas as pd
+df = pd.read_csv('C:\\Users\\renoi\\Session03\\sex ratio.csv', header = None)
 
+# 지역 column 만들기
+r_list = list(df.iloc[:,0])
+r2_list = r_list[1:]
+region_list = []
+for i in r2_list:
+    r = int(i)
+    count = 1
+    while count <= 12:
+        region_list.append(r)
+        count += 1
+print(region_list)
+len(region_list)
+
+# 연도 column 만들기
+y_list = list(df.iloc[0,:])
+y2_list = y_list[1:] * 16
+year_list = []
+for i in y2_list:
+    y = int(i)
+    year_list.append(y)
+print(year_list)
+len(year_list)
+
+# 성비 column 만들기
+import numpy as np
+
+df2 = df.iloc[1:17, 1:13]
+df2_numpy = df2.as_matrix()
+sexratio = df2_numpy.reshape((-1, 1))
+sexratio_list = []
+for [i] in sexratio:
+    sexratio_list.append(i)
+    
+# new dataframe 만들기
+temp = {"지역":region_list,
+        "연도":year_list,
+        "성비":sexratio_list}
+df3 = pd.DataFrame(temp)
+df3
+
+# new csv파일 만들기
+new_df.to_csv('new sex ratio.csv', encoding = 'EUC-KR')
+    
+    
+    
+    
+    
+    
 # 2
 # ‘province data.csv’로부터 지역별로 전 기간에 걸친 평균 gdp와 unemploymen를 구한 뒤, 이를 산포도로 표현하세요.
 # 단, 평균 성비가 110 이상인 지역의 산포도는 파란색 X 모양의 점, 110 미만인 지역의 산포도는 빨간색 세모 모양의 점으로 표현하세요.
